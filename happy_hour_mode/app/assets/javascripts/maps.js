393997c4299a6252cf38b5344776f6a387046851
Sam <samuel1008@gmail.com>
2016-01-28 18:28:23 -0800

    added pw

--- happy_hour_mode/app/assets/javascripts/maps.js
+++ happy_hour_mode/app/assets/javascripts/maps.js
@@ -1,55 +1,55 @@
-$(function() {
+// $(function() {
 	
-		var marketId = []; //returned from the API
-		var allLatlng = []; //returned from the API
-		var allMarkers = []; //returned from the API
-		var marketName = []; //returned from the API
-		var infowindow = null;
-		var pos;
-		var userCords;
-		var tempMarkerHolder = [];
+// 		var marketId = []; //returned from the API
+// 		var allLatlng = []; //returned from the API
+// 		var allMarkers = []; //returned from the API
+// 		var marketName = []; //returned from the API
+// 		var infowindow = null;
+// 		var pos;
+// 		var userCords;
+// 		var tempMarkerHolder = [];
 
 
-		if (navigator.geolocation) {    
+// 		if (navigator.geolocation) {    
 		
-			function error(err) {
-				console.warn('ERROR(' + err.code + '): ' + err.message);
-			}
+// 			function error(err) {
+// 				console.warn('ERROR(' + err.code + '): ' + err.message);
+// 			}
 			
-			function success(pos){
-				userCords = pos.coords;
+// 			function success(pos){
+// 				userCords = pos.coords;
 				
-				//return userCords;
-			}
+// 				//return userCords;
+// 			}
 		
-			// Get the user's current position
-			navigator.geolocation.getCurrentPosition(success, error);
-			//console.log(pos.latitude + " " + pos.longitude);
-			} else {
-				alert('Geolocation is not supported in your browser');
-			}
+// 			// Get the user's current position
+// 			navigator.geolocation.getCurrentPosition(success, error);
+// 			//console.log(pos.latitude + " " + pos.longitude);
+// 			} else {
+// 				alert('Geolocation is not supported in your browser');
+// 			}
 
-		var mapOptions = {
-			zoom: 3,
-			center: new google.maps.LatLng(37.09024, -100.712891),
-			panControl: false,
-			panControlOptions: {
-				position: google.maps.ControlPosition.BOTTOM_LEFT
-			},
-			zoomControl: true,
-			zoomControlOptions: {
-				style: google.maps.ZoomControlStyle.LARGE,
-				position: google.maps.ControlPosition.RIGHT_CENTER
-			},
-			scaleControl: false	
+// 		var mapOptions = {
+// 			zoom: 3,
+// 			center: new google.maps.LatLng(37.09024, -100.712891),
+// 			panControl: false,
+// 			panControlOptions: {
+// 				position: google.maps.ControlPosition.BOTTOM_LEFT
+// 			},
+// 			zoomControl: true,
+// 			zoomControlOptions: {
+// 				style: google.maps.ZoomControlStyle.LARGE,
+// 				position: google.maps.ControlPosition.RIGHT_CENTER
+// 			},
+// 			scaleControl: false	
 
-	infowindow = new google.maps.InfoWindow({
-		content: "holding..."
-	});	
+// 	infowindow = new google.maps.InfoWindow({
+// 		content: "holding..."
+// 	});	
 	
-	//Fire up Google maps and place inside the map-canvas div
-	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);	
+// 	//Fire up Google maps and place inside the map-canvas div
+// 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);	
 
 
 
-});		
+// });		
